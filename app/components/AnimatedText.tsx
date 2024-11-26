0c7f9a9a598569a4c528e8e5dafa4ba54b769f1a
fakepixels <tinaxhe@gmail.com>
2024-11-26 10:59:59 -0500

    fix formatting

--- app/components/AnimatedText.tsx
+++ app/components/AnimatedText.tsx
@@ -2,7 +2,7 @@
 
 const AnimatedText = () => {
   return (
-    <div className="fixed left-8 top-1/2 -translate-y-1/2 -rotate-90 transform origin-left">
+    <div className="fixed left-8 top-[60%] -translate-y-1/2 -rotate-90 transform origin-left">
       <p className="text-3xl text-[#0052ff] dark:text-[#ffffff]">
         This is an <a href="https://onchainkit.xyz" target="_blank" rel="noopener noreferrer" className="hover:text-[#0052ff] dark:hover:text-[#0052ff]"> OnchainKit </a> Demo App
       </p>
